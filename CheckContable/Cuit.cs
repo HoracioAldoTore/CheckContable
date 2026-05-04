7dc4fe3d279b20a3bde808b8bdea806ef2e1129c
Horacio Aldo Tore <96441292+HoracioAldoTore@users.noreply.github.com>
2026-05-03 22:48:24 -0300

    Subida primera

---
+++ CheckContable/Cuit.cs
@@ -0,0 +1,9 @@
+﻿namespace CheckContable
+{
+    public class Cuit
+    {
+        public string ValorOriginal { get; set; }
+        public string ValorCorregido { get; set; }
+        public bool EsValido { get; set; } 
+    }
+}
