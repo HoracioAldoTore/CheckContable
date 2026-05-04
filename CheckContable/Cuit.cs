d992a389384ac0c117d10e5108348c78109d9b0e
Horacio Aldo Tore <96441292+HoracioAldoTore@users.noreply.github.com>
2026-05-03 23:02:57 -0300

    Delete all

--- CheckContable/Cuit.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CheckContable
-{
-    public class Cuit
-    {
-        public string ValorOriginal { get; set; }
-        public string ValorCorregido { get; set; }
-        public bool EsValido { get; set; } 
-    }
-}
